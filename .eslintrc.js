994be46a01fa6c9bb0b319033420e9af60d0bee3
Anton Yashyn <antonyashyn@Antons-MacBook-Air.local>
2025-11-11 23:28:50 +0200

    fix: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'react/no-unstable-nested-components': 'off',
+    'indent': [ 'error', 2 ],
+    'semi': [ 'error', 'never' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'computed-property-spacing': [ 'error', 'always' ],
+    'template-curly-spacing': [ 'error', 'always' ],
+    'space-in-parens': [ 'error', 'always' ],
+  },
+}
--- app/App.tsx
+++ app/App.tsx
@@ -1,45 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+} from 'react-native-safe-area-context'
+import { NewAppScreen } from '@react-native/new-app-screen'
+import { StatusBar, StyleSheet, View } from 'react-native'
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+export const App: React.FunctionComponent = () => (
+  <SafeAreaProvider>
+    <StatusBar barStyle='light-content' />
+    <AppContent />
+  </SafeAreaProvider>
+)
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  const safeAreaInsets = useSafeAreaInsets()
 
   return (
     <View style={styles.container}>
       <NewAppScreen
-        templateFileName="App.tsx"
+        templateFileName='App.tsx'
         safeAreaInsets={safeAreaInsets}
       />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
     flex: 1,
   },
-});
+} )
 
-export default App;
+export default App
